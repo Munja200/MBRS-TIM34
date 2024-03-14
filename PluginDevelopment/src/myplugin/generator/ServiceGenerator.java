cfde8032ee7a3e09d99274b5884aeab984b4f0b9
Mihailo Veljic <93385666+mihailoveljic@users.noreply.github.com>
2024-03-14 20:40:21 +0100

    Added service generator

---
+++ PluginDevelopment/src/myplugin/generator/ServiceGenerator.java
@@ -0,0 +1,60 @@
+package myplugin.generator;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.swing.JOptionPane;
+
+import freemarker.template.TemplateException;
+import myplugin.generator.fmmodel.FMClass;
+import myplugin.generator.fmmodel.FMModel;
+import myplugin.generator.options.GeneratorOptions;
+
+public class ServiceGenerator extends BasicGenerator {
+
+	public ServiceGenerator(GeneratorOptions generatorOptions, String outputPath) {
+		super(generatorOptions);
+		super.setOutputPath(outputPath);
+	}
+
+	public void generate() {
+		try {
+			super.generate();
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(null, e.getMessage());
+		}
+
+		List<FMClass> classes = FMModel.getInstance().getClasses();
+
+		for (int i = 0; i < classes.size(); i++) {
+			FMClass cl = classes.get(i);
+			Writer out;
+
+			Map<String, Object> context = new HashMap<String, Object>();
+			ArrayList<String> imports = new ArrayList<>();
+			String import_str = "";
+			try {
+				if(cl.getPage() != null) {
+					out = getWriter(cl.getName(), cl.getTypePackage());
+					if (out != null) {
+						context.clear();
+						context.put("class", cl);
+						context.put("properties", cl.getProperties());
+						context.put("importedPackages", cl.getImportedPackages());
+						getTemplate().process(context, out);
+						out.flush();
+					}
+				}
+			} catch (TemplateException e) {
+				JOptionPane.showMessageDialog(null, e.getMessage());
+			} catch (IOException e) {
+				JOptionPane.showMessageDialog(null, e.getMessage());
+			}
+		}
+	}
+
+}
